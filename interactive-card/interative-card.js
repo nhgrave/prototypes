f1d1a9d428fc5abcfc04bb7c03f40c8a4cab9a68
Nicolas Grave <nyko-23@hotmail.com>
2024-06-16 18:13:24 -0300

    Move interactive card to a folder.

---
+++ interactive-card/interative-card.js
@@ -0,0 +1,103 @@
+class Card {
+  constructor(imgPath) {
+    this.imgPath = imgPath;
+
+    this.buildElement();
+  }
+
+  buildElement() {
+    const $card = document.createElement('div');
+    $card.classList.add('card');
+    const $img = document.createElement('img');
+    $img.src = this.imgPath;
+    $img.addEventListener('load', this.addEffect.bind(this));
+
+    $card.append($img);
+
+    this.el = $card;
+  }
+
+  addEffect() {
+    this.defineCenterPosition();
+
+    window.addEventListener("resize", () => {
+      this.defineCenterPosition();
+    });
+
+    this.el.addEventListener("mouseover", (event) => {
+      this.defineStyle(event.x, event.y);
+    });
+
+    this.el.addEventListener("mousemove", (event) => {
+      this.defineStyle(event.offsetX, event.offsetY);
+    });
+
+    this.el.addEventListener("mouseleave", () => {
+      this.resetStyle();
+    });
+  }
+
+  defineCenterPosition() {
+    this.centerX = this.el.clientWidth / 2;
+    this.centerY = this.el.clientHeight / 2;
+  }
+
+  defineStyle(mouseOffsetX, mouseOffsetY) {
+    const rotationX = this.rotate(mouseOffsetY, this.centerY, 20);
+    const rotationY = this.rotate(mouseOffsetX, this.centerX, 20);
+    const brightness = 1 - ((rotationX + rotationY) / 200);
+
+    this.el.style.transform = `perspective(1000px) rotateX(${-rotationX}deg) rotateY(${rotationY}deg)`;
+    this.el.style.scale = '1.2';
+    this.el.style.filter = `brightness(${brightness})`;
+    this.el.style.boxShadow = `${-rotationY}px ${rotationX}px 80px 0px rgba(48, 65, 0, 0.5)`;
+
+    this.el.classList.add('animation');
+  }
+
+  resetStyle() {
+    this.el.removeAttribute('style');
+  }
+
+  rotate(cursorPosition, centerPosition, threshold) {
+    let distance = centerPosition - cursorPosition;
+
+    return threshold / centerPosition * distance;
+  }
+};
+
+function buildCards() {
+  const divinationCards = [
+    'the-fool.jpg',
+    'the-magician.jpg',
+    'the-high-priestess.jpg',
+    'the-empress.jpg',
+    'the-emperor.jpg',
+    'the-hierophant.jpg',
+    'the-lovers.jpg',
+    'the-chariot.jpg',
+    'the-strenght.jpg',
+    'the-hermit.jpg',
+    'wheel-of-fortune.jpg',
+    'justice.jpg',
+    'the-hanged-man.jpg',
+    'death.jpg',
+    'temperance.jpg',
+    'the-devil.jpg',
+    'the-tower.jpg',
+    'the-star.jpg',
+    'the-moon.jpg',
+    'the-sun.jpg',
+    'the-judgement.jpg',
+    'the-world.jpg',
+  ];
+
+  const $wrapper = document.querySelector('.content');
+
+  divinationCards.forEach((el) => {
+    const card = new Card(`interative-card-img/${el}`);
+    $wrapper.append(card.el);
+  })
+};
+
+window.onload = buildCards;
---
+++ interactive-card/readme.md
@@ -0,0 +1,3 @@
+# Interactive Card
+
+Card that move in 3D based on mouse hover.
